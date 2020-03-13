41574cf5d26a0eb519fb7a655fb582c0e6fceeb8
Tiago munerato <tiagomunerato@gmail.com>
2020-03-13 11:11:17 -0300

    CRIACAO DE FOREING KEY
    AULA WDSON_13_03_2020

---
+++ DB_Wdson_Foreing_Key.sql
@@ -0,0 +1,80 @@
+/*
+SQLyog Enterprise - MySQL GUI v8.12 
+MySQL - 5.5.5-10.1.34-MariaDB : Database - empresa_animals
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`empresa_animals` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `empresa_animals`;
+
+/*Table structure for table `departamentos` */
+
+DROP TABLE IF EXISTS `departamentos`;
+
+CREATE TABLE `departamentos` (
+  `id_departamento` int(11) NOT NULL AUTO_INCREMENT,
+  `descritivo` varchar(40) NOT NULL,
+  PRIMARY KEY (`id_departamento`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `departamentos` */
+
+/*Table structure for table `empresas` */
+
+DROP TABLE IF EXISTS `empresas`;
+
+CREATE TABLE `empresas` (
+  `id_empresa` int(11) NOT NULL AUTO_INCREMENT,
+  `descritivo` varchar(40) NOT NULL,
+  PRIMARY KEY (`id_empresa`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `empresas` */
+
+/*Table structure for table `funcionarios` */
+
+DROP TABLE IF EXISTS `funcionarios`;
+
+CREATE TABLE `funcionarios` (
+  `id_funcionario` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(40) NOT NULL,
+  `CPF` char(11) DEFAULT NULL,
+  `Salario` decimal(8,2) NOT NULL,
+  `Logradouro` varchar(40) NOT NULL,
+  `Cidade` varchar(40) NOT NULL,
+  `Uf` char(2) NOT NULL,
+  `Sexo` enum('F','M') DEFAULT NULL,
+  `id_empresa` int(11) NOT NULL,
+  `id_departamento` int(11) NOT NULL,
+  PRIMARY KEY (`id_funcionario`),
+  UNIQUE KEY `CPF` (`CPF`),
+  KEY `id_empresa` (`id_empresa`),
+  KEY `id_departamento` (`id_departamento`),
+  CONSTRAINT `funcionarios_ibfk_1` FOREIGN KEY (`id_empresa`) REFERENCES `empresas` (`id_empresa`),
+  CONSTRAINT `funcionarios_ibfk_2` FOREIGN KEY (`id_departamento`) REFERENCES `departamentos` (`id_departamento`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `funcionarios` */
+
+/*Table structure for table `projetos` */
+
+DROP TABLE IF EXISTS `projetos`;
+
+CREATE TABLE `projetos` (
+  `id_projeto` int(11) NOT NULL AUTO_INCREMENT,
+  `descritivo` varchar(40) NOT NULL,
+  PRIMARY KEY (`id_projeto`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `projetos` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
